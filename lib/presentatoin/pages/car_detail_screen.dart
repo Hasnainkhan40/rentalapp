f71f6c76bcedce0a692a906b8f96bbefb9c2cea3
Hasnain Khan <mohasnainkhan148@gmail.com>
2025-07-25 23:12:08 +0530

    first commit

---
+++ lib/presentatoin/pages/car_detail_screen.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:rentalapp/data.model/car.dart';
+import 'package:rentalapp/presentatoin/pages/mapsDetailpage.dart';
+import 'package:rentalapp/presentatoin/widgets/car_card.dart';
+import 'package:rentalapp/presentatoin/widgets/more_card.dart';
+
+class CarDetailScreen extends StatefulWidget {
+  final Car car;
+  const CarDetailScreen({super.key, required this.car});
+
+  @override
+  State<CarDetailScreen> createState() => _CarDetailScreenState();
+}
+
+class _CarDetailScreenState extends State<CarDetailScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 1.0, end: 1.5).animate(_controller!)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _controller!.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller!.forward();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 55),
+          child: Row(
+            children: [Icon(Icons.info_outline), Text(' Information')],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          CarCard(
+            car: Car(
+              model: widget.car.model,
+              distance: widget.car.distance,
+              fuelCapacity: widget.car.fuelCapacity,
+              pricePerHour: widget.car.pricePerHour,
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Color(0xffF3F3F3),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10,
+                          spreadRadius: 5,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 40,
+                          backgroundImage: AssetImage('assets/user.png'),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'user123',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text('\$4.256', style: TextStyle(color: Colors.grey)),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Mapsdetailpage(car: widget.car),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      height: 170,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 10,
+                            spreadRadius: 5,
+                          ),
+                        ],
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Transform.scale(
+                          scale: _animation!.value,
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            'assets/maps.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                SizedBox(height: 20),
+                MoreCard(
+                  car: Car(
+                    model: widget.car.model + "-1",
+                    distance: widget.car.distance + 100,
+                    fuelCapacity: widget.car.fuelCapacity + 100,
+                    pricePerHour: widget.car.pricePerHour + 10,
+                  ),
+                ),
+                SizedBox(height: 5),
+                MoreCard(
+                  car: Car(
+                    model: widget.car.model + "-1",
+                    distance: widget.car.distance + 200,
+                    fuelCapacity: widget.car.fuelCapacity + 200,
+                    pricePerHour: widget.car.pricePerHour + 20,
+                  ),
+                ),
+                SizedBox(height: 5),
+                MoreCard(
+                  car: Car(
+                    model: widget.car.model + "-1",
+                    distance: widget.car.distance + 300,
+                    fuelCapacity: widget.car.fuelCapacity + 300,
+                    pricePerHour: widget.car.pricePerHour + 30,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentatoin/pages/mapsDetailpage.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:rentalapp/data.model/car.dart';
+import 'package:rentalapp/presentatoin/pages/car_detail_screen.dart';
+
+class Mapsdetailpage extends StatelessWidget {
+  final Car car;
+
+  const Mapsdetailpage({super.key, required this.car});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+      ),
+      body: Stack(
+        children: [
+          FlutterMap(
+            options: MapOptions(
+              initialCenter: LatLng(26.9124, 75.7873), // Center of India
+              initialZoom: 5.5, // Zoomed out enough to view whole country
+            ),
+            children: [
+              TileLayer(
+                urlTemplate:
+                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                subdomains: ['a', 'b', 'c'],
+                tileProvider: NetworkTileProvider(
+                  headers: {
+                    'User-Agent':
+                        'flutter_map_example/1.0 (https://yourapp.com)',
+                  },
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: carDetailCard(car: car),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget carDetailCard({required Car car}) {
+  return SizedBox(
+    height: 350,
+    child: Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.black54,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+            boxShadow: [
+              BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text(
+                '${car.model}',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Icon(Icons.directions_car, color: Colors.white, size: 16),
+                  SizedBox(width: 5),
+                  Text(
+                    '> ${car.distance} km',
+                    style: TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                  SizedBox(width: 10),
+                  Icon(Icons.battery_full, color: Colors.white, size: 14),
+                  SizedBox(height: 10),
+                  Text(
+                    '> ${car.fuelCapacity.toString()}',
+                    style: TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Features",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                featureIcons(),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '\$${car.pricePerHour}/day',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                      ),
+                      child: Text(
+                        'Book Now',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          top: 50,
+          right: 20,
+          child: Image.asset('assets/white_car.png'),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget featureIcons() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      featureIcon(Icons.local_gas_station, 'Diesel', 'Common Rail'),
+      featureIcon(Icons.speed, 'Acceleration', '0 - 100km/s'),
+      featureIcon(Icons.ac_unit, 'Cold', 'Temp Control'),
+    ],
+  );
+}
+
+Widget featureIcon(IconData icon, String title, String subtitle) {
+  return Container(
+    width: 100,
+    height: 100,
+    padding: EdgeInsets.all(5),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      border: Border.all(color: Colors.grey, width: 1),
+    ),
+    child: Column(
+      children: [
+        Icon(icon, size: 28),
+        Text(title),
+        Text(subtitle, style: TextStyle(color: Colors.grey, fontSize: 10)),
+      ],
+    ),
+  );
+}
